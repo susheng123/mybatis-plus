ce6fe27c659c6cc8cd1a82e00bc05c3a0e3547e3
susheng123 <243728155@qq.com>
2018-08-15 21:46:11 +0800

    demo2

---
+++ mp02/src/main/java/com/atguigu/mp/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.mp.mapper;
+
+import com.atguigu.mp.beans.Employee;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author susheng
+ * @since 2018-08-15
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
---
+++ mp02/src/main/java/com/atguigu/mp/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.atguigu.mp.service;
+
+import com.atguigu.mp.beans.Employee;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author susheng
+ * @since 2018-08-15
+ */
+public interface EmployeeService extends IService<Employee> {
+
+}
